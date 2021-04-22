4fa5267530943ce3acc28701346dc3213ab71881
Hviid1 <201906104@post.au.dk>
2021-04-22 13:35:34 +0200

    Added Step1

--- Microwave.App/Program.cs
+++ Microwave.App/Program.cs
@@ -22,7 +22,7 @@ namespace Microwave.App
 
             Light light = new Light(output);
 
-            Microwave.Classes.Boundary.Timer timer = new Timer();
+            Timer timer = new Timer();
 
             CookController cooker = new CookController(timer, display, powerTube);
 
---
+++ Microwave.Test.Integration/Step1.cs
@@ -0,0 +1,32 @@
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Interfaces;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class Step1
+    {
+        private IOutput _output;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ILight _light;
+
+        [SetUp]
+        public void Setup()
+        {
+            _output = new Output();
+            _display = new Display(_output);
+            _powerTube = new PowerTube(_output);
+            _light = new Light(_output);
+        }
+
+        [Test]
+        public void Test_TurnOn_CorrectOutput()
+        {
+            _light.TurnOn();
+
+
+        }
+    }
+}
