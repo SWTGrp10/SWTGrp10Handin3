ff9672c3907bbce533c570afff92a90f1a85178d
Hviid1 <201906104@post.au.dk>
2021-04-22 14:05:56 +0200

    Integration test step1 display and light done

--- Microwave.Test.Integration/Step1.cs
+++ Microwave.Test.Integration/Step1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Microwave.Classes.Boundary;
 using Microwave.Classes.Interfaces;
 using NUnit.Framework;
@@ -11,6 +13,7 @@ namespace Microwave.Test.Integration
         private IDisplay _display;
         private IPowerTube _powerTube;
         private ILight _light;
+        private StringWriter _stringWriter;
 
         [SetUp]
         public void Setup()
@@ -19,14 +22,55 @@ namespace Microwave.Test.Integration
             _display = new Display(_output);
             _powerTube = new PowerTube(_output);
             _light = new Light(_output);
+            _stringWriter = new StringWriter();
         }
 
         [Test]
-        public void Test_TurnOn_CorrectOutput()
+        public void TestLight_TurnOn_CorrectOutput()
         {
+            Console.SetOut(_stringWriter);
             _light.TurnOn();
 
+            Assert.That(_stringWriter.ToString().Contains("on") && _stringWriter.ToString().Contains("Light"));
+        }
 
+        [Test]
+        public void TestLight_TurnOff_CorrectOutput()
+        {
+            _light.TurnOn();
+            Console.SetOut(_stringWriter);
+            _light.TurnOff();
+
+            Assert.That(_stringWriter.ToString().Contains("off") && _stringWriter.ToString().Contains("Light"));
+        }
+
+        [TestCase(20,15)]
+        [TestCase(1,45)]
+        public void TestDisplay_ShowTime_CorrectOutput(int min, int sec)
+        {
+            Console.SetOut(_stringWriter);
+            _display.ShowTime(min, sec);
+
+            Assert.That(_stringWriter.ToString().Contains(min.ToString()) && _stringWriter.ToString().Contains(sec.ToString()));
+        }
+
+        [TestCase(20)]
+        [TestCase(1)]
+        public void TestDisplay_ShowPower_CorrectOutput(int power)
+        {
+            Console.SetOut(_stringWriter);
+            _display.ShowPower(power);
+
+            Assert.That(_stringWriter.ToString().Contains(power.ToString()) && _stringWriter.ToString().Contains("W"));
+        }
+
+        [Test]
+        public void TestDisplay_Clear_CorrectOutput()
+        {
+            Console.SetOut(_stringWriter);
+            _display.Clear();
+
+            Assert.That(_stringWriter.ToString().Contains("cleared"));
         }
     }
 }
